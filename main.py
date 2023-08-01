d320455fd06546e3421d617e7bd1f2d55eac2f3b
Stepan D <h3xp3ctb@gmail.com>
2023-08-01 18:08:45 +0300

    01.08.2023 work

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from ui.base_qt_ui.ui_graph import Ui_Graph
 
 class Graph(QtWidgets.QMainWindow):
 
-    def __init__(self):
+    def __init__(self, color, x, y):
         QtWidgets.QMainWindow.__init__(self)
         self.ui = Ui_Graph()
         self.ui.setupUi(self)
@@ -16,11 +16,16 @@ class Graph(QtWidgets.QMainWindow):
         self.level = 0
         self.form_button()
         self.ui.button_reset.clicked.connect(self.buttons_reset)
+        self.color = color
+        self.x_coord = x
+        self.y_coord = y
+        print(x)
+        print(self.x_coord)
 
     def change_button_color(self):
         button = app.focusWidget()  # Получаем кнопку, на которую нажали
-        if button.palette().button().color().name() == '#ffffff':
-            button.setStyleSheet("background-color: black")
+        if button.palette().button().color().name() == '#ff0000':
+            button.setStyleSheet("background-color: red")
         else:
             button.setStyleSheet("background-color: white")
         self.draw_hor_line(button.objectName())
@@ -37,13 +42,13 @@ class Graph(QtWidgets.QMainWindow):
         row = number // 220
         if self.end < col:
             for i in range(self.end, col):
-                self.buttons[i + row * 220].setStyleSheet("background-color: black")
+                self.buttons[i + row * 220].setStyleSheet("background-color: red")
             if self.level > row:
                 for j in range(row, self.level):
-                    self.buttons[j*220 + self.end].setStyleSheet("background-color: black")
+                    self.buttons[j*220 + self.end].setStyleSheet("background-color: red")
             elif self.level < row:
                 for j in range(self.level, row):
-                    self.buttons[j*220 + self.end].setStyleSheet("background-color: black")
+                    self.buttons[j*220 + self.end].setStyleSheet("background-color: red")
             self.end = col
             self.level = row
 
@@ -51,7 +56,7 @@ class Graph(QtWidgets.QMainWindow):
         for row in range(6):
             for col in range(220):
                 button = QPushButton(f"", self)
-                button.setGeometry(10 + 5 * col, 10 + 5 * row, 5, 5)
+                button.setGeometry(self.y_coord + 5 * col, self.x_coord + 5 * row, 5, 5)
                 button.setObjectName(f'button{str(col + 220 * row).zfill(4)}')
                 button.setStyleSheet(f"background-color: white")
                 button.clicked.connect(self.change_button_color)
@@ -60,6 +65,8 @@ class Graph(QtWidgets.QMainWindow):
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    main = Graph()
-    main.show()
+    graph1 = Graph('red', 10, 10)
+    graph2 = Graph('blue', 60, 10)
+    graph1.show()
+    graph2.show()
     sys.exit(app.exec())
