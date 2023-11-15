79160008f44c53a02eab75851b67f023414d3fc2
Stepan D. <h3xp3ctb@gmail.com>
2023-11-15 22:11:58 +0200

    15.11.2023 home

--- tests/find_axle_valid.py
+++ tests/find_axle_valid.py
@@ -240,41 +240,47 @@ class control_axle_test(TestCase):
 		self.assertEqual(find_axle.get_count_sys1(), count_sys2_1)
 		self.assertEqual(find_axle.get_count_sys2(), count_sys1_1)
 
-
-
-	"""
-	def test_0010(self):  #
-		tmp = f"SYS1(1:100µs; 0:1100µs; 1:1300µs; 0:2300µs; 1:2600µs; 0:3600µs; 1:4100µs); SYS2(1:500µs; 0:4000µs; 1:4100µs)"
+	def test_0015(self): # three part
+		data = f"SYS1(1:1000µs; 0:1600µs); SYS2(1:1400µs; 0:1600µs)"
+		data_1 = f"SYS1(0:1600µs); SYS2(0:1600µs)"
+		data_2 = f"SYS1(0:400µs; 1:1600µs); SYS2(0:800µs; 1:1600µs)"
+		count_axle = 0
+		count_sys1 = 0
+		count_sys2 = 0
+		count_axle_1 = 0
+		count_sys1_1 = 0
+		count_sys2_1 = 0
+		count_axle_2 = 1
+		count_sys1_2 = 1
+		count_sys2_2 = 1
 		find_axle = FindAxle()
-		find_axle.work_find_axle(tmp)
-		self.assertEqual(find_axle.get_count_axle(), 1)
-		self.assertEqual(find_axle.get_count_sys1(), 3)
-		self.assertEqual(find_axle.get_count_sys2(), 1)
+		find_axle.work_find_axle(data)
+		self.assertEqual(find_axle.get_count_axle(), count_axle)
+		self.assertEqual(find_axle.get_count_sys1(), count_sys1)
+		self.assertEqual(find_axle.get_count_sys2(), count_sys2)
+		find_axle.work_find_axle(data_1)
+		self.assertEqual(find_axle.get_count_axle(), count_axle_1)
+		self.assertEqual(find_axle.get_count_sys1(), count_sys1_1)
+		self.assertEqual(find_axle.get_count_sys2(), count_sys2_1)
+		find_axle.work_find_axle(data_2)
+		self.assertEqual(find_axle.get_count_axle(), count_axle_2)
+		self.assertEqual(find_axle.get_count_sys1(), count_sys1_2)
+		self.assertEqual(find_axle.get_count_sys2(), count_sys2_2)
+		find_axle.reset()
+		find_axle.work_find_axle(data.replace('SYS1', 'SYS3').replace('SYS2', 'SYS1').replace('SYS3', 'SYS2'))
+		self.assertEqual(find_axle.get_count_axle(), count_axle)
+		self.assertEqual(find_axle.get_count_sys1(), count_sys2)
+		self.assertEqual(find_axle.get_count_sys2(), count_sys1)
+		find_axle.work_find_axle(data_1.replace('SYS1', 'SYS3').replace('SYS2', 'SYS1').replace('SYS3', 'SYS2'))
+		self.assertEqual(find_axle.get_count_axle(), -count_axle_1)
+		self.assertEqual(find_axle.get_count_sys1(), count_sys2_1)
+		self.assertEqual(find_axle.get_count_sys2(), count_sys1_1)
+		find_axle.work_find_axle(data_2.replace('SYS1', 'SYS3').replace('SYS2', 'SYS1').replace('SYS3', 'SYS2'))
+		self.assertEqual(find_axle.get_count_axle(), -count_axle_2)
+		self.assertEqual(find_axle.get_count_sys1(), count_sys2_2)
+		self.assertEqual(find_axle.get_count_sys2(), count_sys1_2)
 
-	def test_0011(self):  #
-		tmp = f"SYS2(1:100µs; 0:1100µs; 1:1300µs; 0:2300µs; 1:2600µs; 0:3600µs; 1:4100µs); SYS1(1:500µs; 0:4000µs; 1:4100µs)"
-		find_axle = FindAxle()
-		find_axle.work_find_axle(tmp)
-		self.assertEqual(find_axle.get_count_axle(), -1)
-		self.assertEqual(find_axle.get_count_sys1(), 1)
-		self.assertEqual(find_axle.get_count_sys2(), 3)
 
-	def test_0012(self):  #
-		tmp = f"SYS1(1:100µs; 0:1100µs; 1:1300µs; 0:2300µs; 1:2600µs; 0:3600µs; 1:4100µs); SYS2(1:500µs; 0:3200µs; 1:4100µs)"
-		find_axle = FindAxle()
-		find_axle.work_find_axle(tmp)
-		self.assertEqual(find_axle.get_count_axle(), 0)
-		self.assertEqual(find_axle.get_count_sys1(), 3)
-		self.assertEqual(find_axle.get_count_sys2(), 1)
-
-	def test_0013(self):  #
-		tmp = f"SYS2(1:100µs; 0:1100µs; 1:1300µs; 0:2300µs; 1:2600µs; 0:3600µs; 1:4100µs); SYS1(1:500µs; 0:3200µs; 1:4100µs)"
-		find_axle = FindAxle()
-		find_axle.work_find_axle(tmp)
-		self.assertEqual(find_axle.get_count_axle(), 0)
-		self.assertEqual(find_axle.get_count_sys1(), 1)
-		self.assertEqual(find_axle.get_count_sys2(), 3)
-	"""
 
 if __name__ == '__main__':
 	main()
